4f78b2be77bbded7352c0d3fccef228ae7f76ba6
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2025-04-05 23:06:03 +0530

    2weeks

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -9,8 +9,7 @@
 // - loops
 // - vectors
 // - files
-// - user input
-// We've tried to limit/hide Rust's quirks since we'll discuss those details
+// - user input We've tried to limit/hide Rust's quirks since we'll discuss those details
 // more in depth in the coming lectures.
 extern crate rand;
 use rand::Rng;
@@ -34,7 +33,64 @@ fn main() {
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+    //println!("random word: {}", secret_word);
 
     // Your code here! :)
+    let mut guess_string: Vec<i32> = vec![1; secret_word_chars.len()];
+
+    let mut correct_guess = false;
+    let mut i = NUM_INCORRECT_GUESSES;
+
+    let mut total: i32 = 100;
+    while i > 0 {
+        println!("You have {} guesses left", i);
+
+        print!("Give a guess: ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line");
+
+        if guess.len() as i32 != 2 {
+            println!("Please input only 1 character");
+            continue;
+        }
+
+        for (i, c) in secret_word.chars().enumerate() {
+            //println!("guess: {}, char: {}",guess.trim(), c.to_string()==guess);
+            if guess.trim() == c.to_string() && guess_string[i] == 1 {
+                guess_string[i] = 0;
+                correct_guess = true;
+                println!("You guess {} correctly as position {}!", c, i + 1);
+                break;
+            }
+        }
+
+        i -= 1;
+        if correct_guess {
+            correct_guess = false;
+            i += 1;
+        }
+        let mut sofar: String = String::with_capacity(secret_word.len());
+        for (i, guessed) in guess_string.iter().enumerate() {
+            if *guessed == 0 {
+                sofar.push(secret_word.chars().nth(i).unwrap());
+            } else {
+                sofar.push_str("-");
+            }
+        }
+        println!("Guessed so far {}", sofar);
+        total = guess_string.iter().sum();
+        if total == 0 {
+            break;
+        }
+    }
+
+    if total == 0 {
+        println!("You guessed the whole word correctly");
+    } else {
+        println!("You weren't able to get it this time :(");
+    }
 }
